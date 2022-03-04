60565f1e7fa1405f4750f6f57eff871dde928e9b
azotamiota <azotamiota@gmail.com>
2022-03-04 20:53:27 +0100

    dropdown menu fixed

--- bls_instructions/bls_instruction.js
+++ bls_instructions/bls_instruction.js
@@ -1,16 +1,21 @@
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show")
 }
 
 window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
+  if (!event.target.matches(".dropbtn") && !event.target.matches(".fas")) {
+    console.log("clicked outside of the button");
+    if (
+      document
+        .getElementsByClassName("dropdown-content")[0]
+        .classList.contains("show")
+    ) {
+      document
+        .getElementsByClassName("dropdown-content")[0]
+        .classList.remove("show");
     }
+
+    // for (let i = 0; i < dropdowns.length; i++) {
+    //   var openDropdown = dropdowns[i];
   }
 };
