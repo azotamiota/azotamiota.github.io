10db52ab0c003cea2cb4c8be0a24a6230bd3b59f
azotamiota <azotamiota@gmail.com>
2022-07-01 16:29:35 +0100

    delete basic projects

--- US-phone-number-checker/us-phone-num-valid.js
+++
@@ -1,9 +0,0 @@
-function telephoneCheck(str) {
-    //const charsToRemove = /[-()]/;
-    const regEx = /^1?\s?(\([0-9]{3}\)|[0-9]{3})\s?\-?[0-9]{3}\-?\s?[0-9]{4}$/g;
-    
-     if (str.match(regEx)) {return 'Yes, it\'s a valid US phone number';}
-      else {return 'No, it\'s not a valid US phone number';}
-      
-    }
-    
